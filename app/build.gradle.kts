65d6a716926a21c788057a2a369afdb1a1aa9d73
Antonino Ardines <antonino.ardines@gmail.com>
2024-02-09 07:01:03 -0600

    Removed incorrect dependency in main module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 
 dependencies {
 
-    implementation("project:model")
+
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
